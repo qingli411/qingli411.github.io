3d84a779b8e92b371fc0ba39c6c7d3a60b873c3e
Qing Li <lqingpku@gmail.com>
2020-03-08 14:43:18 -0600

    Add presentations

--- pubs/update_pubs.sh
+++ pubs/update_pubs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./bib2xhtml -c -r -R -s plain -n "Qing Li" MyPubs.bib mypubs.html
+./bib2xhtml -c -r -R -s plain -n "Qing Li" publications.bib publications.html
+./bib2xhtml -c -r -R -s plain -n "Qing Li" presentations.bib presentations.html
